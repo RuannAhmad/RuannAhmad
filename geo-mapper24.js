df835079a16150cf2cf6a92d3b86af9ab0e7de82
geo-mapper <evkipchumba3@gmail.com>
2024-05-14 18:33:44 +0300

    Update geo-mapper24.js

--- geo-mapper24.js
+++ geo-mapper24.js
@@ -113,7 +113,7 @@ fetch('https://geo-mapper.github.io/geo-mapper/gmeveningroute.geojson')
 
 
 // Load the GeoJSON polygon file
-fetch('https://geo-mapper.github.io/geo-mapper/gmbuildings.geojson')
+fetch('https://geo-mapper.github.io/geo-mapper/gmmorningroutebuildings.geojson')
 .then(response => response.json())
 .then(geojson => {
     // Customize the style of the polygon
